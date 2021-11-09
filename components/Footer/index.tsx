c5bb236a8c9dec8c0a621e1248f67f68293b8c5f
lgu0521 <lgu0521@gmail.com>
2021-11-09 23:08:33 +0900

    footer 수정

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styled from 'styled-components';
+import { Content } from "../GlobalComponents";
 
 
 const Wrap = styled.div`
@@ -17,15 +18,7 @@ const Footer = () => {
     return (
         <nav>
             <Wrap>
-                <p>
-                회사명 : ㈜샐러디
-                </p>
-                <p>
-                사업자등록번호 : 681-86-00031
-                </p>
-                <p>
-                COPYRIGHT(C) 2021 ㈜샐러디. CO.LTD ALL RIGHT RESERVED.
-                </p>
+                <Content style={{color:"white"}}>회사명 비오키친 사업자등록번호 856-35-00894 COPYRIGHT(C) 2021 비오키친. CO.LTD ALL RIGHT RESERVED</Content>
             </Wrap>
         </nav>
     );
