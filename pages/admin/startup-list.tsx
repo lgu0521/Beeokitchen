6d39f7de680338a1220541fe4c8140b0d8c88f5f
lgu0521 <lgu0521@gmail.com>
2021-11-24 16:25:24 +0900

    ..

--- pages/admin/startup-list.tsx
+++ pages/admin/startup-list.tsx
@@ -1,44 +1,44 @@
-import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { StartUpFormDTO } from "../../dto/startup-form.dto";
+// import { GetStaticProps, InferGetStaticPropsType } from "next";
+// import { StartUpFormDTO } from "../../dto/startup-form.dto";
 
-const StartupList = (Props: InferGetStaticPropsType<typeof getStaticProps>) => {
-    const formList: StartUpFormDTO[] = Props.sendData;
-    console.log(Props);
-    return (
-        <div>
+// const StartupList = (Props: InferGetStaticPropsType<typeof getStaticProps>) => {
+//     const formList: StartUpFormDTO[] = Props.sendData;
+//     console.log(Props);
+//     return (
+//         <div>
 
-            {formList.map((item, key) => (
-                <div key={key}>
-                    <p>{item.name}</p>
-                    <p>{item.phonenumber}</p>
-                    <p>{item.area}</p>
-                    <p>{item.experience}</p>
-                    <p>{item.money}</p>
-                    <p>{item.openday}</p>
-                </div>
-            ))
-            }
+//             {formList.map((item, key) => (
+//                 <div key={key}>
+//                     <p>{item.name}</p>
+//                     <p>{item.phonenumber}</p>
+//                     <p>{item.area}</p>
+//                     <p>{item.experience}</p>
+//                     <p>{item.money}</p>
+//                     <p>{item.openday}</p>
+//                 </div>
+//             ))
+//             }
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
 
-export const getStaticProps: GetStaticProps = async () => {
-    const resData = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/startup-form");
-    const sendData = await resData.json();
-    console.log(sendData);
-    if (!sendData) {
-        return {
-            notFound: true
-        }
-    };
+// export const getStaticProps: GetStaticProps = async () => {
+//     const resData = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/startup-form");
+//     const sendData = await resData.json();
+//     console.log(sendData);
+//     if (!sendData) {
+//         return {
+//             notFound: true
+//         }
+//     };
 
-    return {
-        props: {
-            sendData
-        }
-    }
-}
+//     return {
+//         props: {
+//             sendData
+//         }
+//     }
+// }
 
-export default StartupList;
+// export default StartupList;
