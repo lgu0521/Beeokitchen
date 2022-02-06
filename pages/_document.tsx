a62559b3f3b35994383ea5dfeb5db94ba5d09208
lgu0521 <lgu0521@gmail.com>
2022-02-06 23:16:49 +0900

    tnwjd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,15 +11,15 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <meta charSet="utf-8" />
-                    <link rel="shortcut icon" sizes="48x48" href="https://www.xn--9w3b27lmmhzmc.kr/favicon.ico"/>
-                    <link rel="icon" href="https://www.xn--9w3b27lmmhzmc.kr/favicon.png"/>
+                    <link rel="shortcut icon" sizes="48x48" href="/favicon.ico"/>
+                    <link rel="icon" href="/favicon.png"/>
                     <meta name="google-site-verification" content="AMDIh0U1uc2qnbti6OVyxiOuehBj6BTGk0NERU9yums" />
                     <meta name="naver-site-verification" content="318a146517bed2f8a2764ebd615cba64c31a5b60" />
                     <meta property="og:type" content="website" />
                     <meta property="og:title" content="비오키친" />
                     <meta property="og:site_name" content="다이어트푸드 전문키친" />
                     <meta property="og:url" content="https://www.xn--9w3b27lmmhzmc.kr/brand" />
-                    <meta property="og:image" content="story-sub1.png" />
+                    <meta property="og:image" content="/story-sub1.png" />
                     <meta property="og:image:width" content="800" />
                     <meta property="og:image:height" content="400" />
                     <meta property="og:description" content="건강한 식습관, 비오키친" key="description" />
